81134a382bf6a7c223a684691600e9d51a83a9f5
hugo1911 <hugokolbe6a@outlook.com>
2025-01-27 20:15:46 -0800

    Initial Commit

---
+++ ManejadorDeTareas/ManejadorDeTareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManejadorDeTareasApp.swift
+//  ManejadorDeTareas
+//
+//  Created by Hugo Manzano on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ManejadorDeTareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
